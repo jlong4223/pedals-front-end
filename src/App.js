12d824aad67db6a584509fbb4f2aed8b493da932
jared Long <jlong4223@gmail.com>
2020-12-07 22:52:30 -0600

    imports hooks, header component and notes/TODOS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
 import './App.css';
+
+// imports hooks
+import {useState, useEffect} from 'react'
+import {Link, Route, Switch} from 'react-router-dom'
+
+// TODO import DB / API functions
+
+
+// TODO import components
+import Header from './components/Header/Header'
+
+
+// TODO create a 404 page function
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Hey There
-      </header>
+      <Header />
     </div>
   );
 }

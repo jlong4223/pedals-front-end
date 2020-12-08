14b7b800e2f30b0b81d9c2316aac97aeea0194dd
jared Long <jlong4223@gmail.com>
2020-12-08 14:03:29 -0600

    imports hooks, modules, pages, adds routes, sets state

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import {useState, useEffect} from 'react'
 import {Link, Route, Switch} from 'react-router-dom'
 
 // TODO import DB / API functions
+import {fetchBikeData} from './services/bike-api'
 
-
-// TODO import components
+// TODO import components/pages
+import HomePage from './pages/HomePage/HomePage'
+import SignupPage from './pages/SignupPage/SignupPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 
 // TODO create a 404 page function
@@ -16,9 +20,39 @@ import Header from './components/Header/Header'
 
 
 function App() {
+
+  const [bikeData, setBikeData]= useState([{
+    name: '',
+    type: '',
+    location: '',
+    brand: '',
+    size: '',
+    availableTill: '',
+    imgURL: ''
+  }])
+
+  async function getData(){
+    const data = await fetchBikeData()
+    setBikeData(data)
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
     <div className="App">
-      <Header />
+      <Switch>
+        <Route exact path="/" render={()=>
+          <HomePage />
+        }/>
+        <Route exact path="/signup" render={props => 
+            <SignupPage {...props} />
+          }/>
+          <Route exact path="/login" render={props => 
+            <LoginPage {...props} />
+          }/>
+      </Switch>
     </div>
   );
 }

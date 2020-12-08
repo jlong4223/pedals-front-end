42613dbf5f2c23feaea735212c817997278a90d9
jared Long <jlong4223@gmail.com>
2020-12-08 14:04:53 -0600

    fetchs data from backend

--- src/services/bike-api.js
+++ src/services/bike-api.js
@@ -1,1 +1,7 @@
 // TODO make AJAX requests to my bike back end
+const BASE_URL = 'http://localhost:3001/bikes'
+
+export function fetchBikeData(){
+    return fetch(BASE_URL).then(res=>res.json())
+}
+

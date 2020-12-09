a2d9b0e4d01b54ea12df92c0c51cb564279d22dd
jared Long <jlong4223@gmail.com>
2020-12-09 14:44:25 -0600

    removes old comments

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import './HomePage.css'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import About from '../../components/About/About'
 
 const HomePage = (props)=>{

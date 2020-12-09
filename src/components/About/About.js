de411ea7eb26d0b5f67db343dd98d70428a5ce06
jared Long <jlong4223@gmail.com>
2020-12-08 22:53:59 -0600

    adds about component

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+const About = (props)=>{
+    return(
+        <div>
+            <h1>Pedal your way through the city</h1>
+            <p>Book unforgettable bikes from trusted hosts in Austin, TX</p>
+        </div>
+    )
+}
+
+export default About

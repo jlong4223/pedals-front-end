dd0cdf6ada91fa854c1888194dc7568807df7c2e
jared Long <jlong4223@gmail.com>
2020-12-08 22:54:31 -0600

    adds bike page as a link and route

---
+++ src/pages/BikesPage/BikesPage.js
@@ -0,0 +1,9 @@
+const BikesPage = (props)=>{
+    return(
+        <div>
+            <h1>Bikes</h1>
+        </div>
+    )
+}
+
+export default BikesPage
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,26 @@
-import NavBar from '../../components/NavBar/NavBar'
-// import Header from '../../components/Header/Header'
+import './HomePage.css'
+import {Link} from 'react-router-dom'
+import About from '../../components/About/About'
 
 const HomePage = (props)=>{
     return(
         <main className="page">
-            {/* <Header /> */}
-            {/* <NavBar /> */}
-            <h1>HomePage</h1>
+            <div>
+                <About />
+            </div>
+            <div className="homeLinks">
+                <div>
+                <Link to="/bikes">
+                   <h1> Bikes Available </h1>
+                </Link>  
+                </div>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <div>
+                <Link to='/trails'>
+                    <h1> Trails </h1>
+                </Link>    
+                </div>
+            </div>
         </main>
     )
 }

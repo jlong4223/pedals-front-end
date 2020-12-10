c12f18ce51ac484dbbfd95e82aa79828e0e78cf0
jared Long <jlong4223@gmail.com>
2020-12-10 14:35:33 -0600

    adds working post functionality

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ const Footer = (props)=>{
     return(
     <footer className="Footer">Copyright &copy; Pedals {new Date().getFullYear()} All Rights Reserved
     &nbsp;&nbsp;
-    <a href="https://www.linkedin.com/in/jaredlong-95/" target='_blank'><i class="fab fa-linkedin"></i></a>
+    <a href="https://www.linkedin.com/in/jaredlong-95/" target='_blank' rel="noreferrer"><i class="fab fa-linkedin"></i></a>
     &nbsp;&nbsp;
-    <a href="https://github.com/jlong4223" target='_blank'><i class="fab fa-github-square"></i></a>
+    <a href="https://github.com/jlong4223" target='_blank' rel="noreferrer"><i class="fab fa-github-square"></i></a>
     </footer>
     )
-}
+}  
 
 export default Footer 
--- src/pages/NewBikePage/NewBikePage.js
+++ src/pages/NewBikePage/NewBikePage.js
@@ -2,40 +2,27 @@ import {Link} from 'react-router-dom'
 import{addBikeData} from '../../services/bike-api'
 
 // question do I need to have state here and have a setState?
-    //tested postData with webhook https://webhook.site/#!/c4305721-ec14-49d5-aee6-6d3335cf390d
-async function handleSubmit(e){
-    e.preventDefault()
-    addBikeData()
-    // props.history.push('/bikes')
-}
 
-async function postData(e){  
-    e.preventDefault()
-    try{
-        let result = await fetch('http://localhost:3001/bikes', {
-            method: 'POST', 
-            mode: 'no-cors', 
-            headers:{
-                'Accept': 'application/json',
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                name:'',
-                type: '',
-                location: '',
-                brand: '',
-                size: '',
-                availableTill: '',
-                imgURL: ''
-            })
-        })
-        console.log('Result' + result)
-    } catch(e){
-        console.log(e)
-    }
-}
 
 const NewBikePage = (props)=>{
+    async function postData(e,bikes){  
+        e.preventDefault()
+        try{
+            let result = await fetch('http://localhost:3001/bikes', {
+                method: 'POST', 
+                mode: 'no-cors', 
+                headers:{
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({bikes})
+            })
+            console.log('Result' + result)
+            // props.history.push('/bikes')
+        } catch(e){
+            console.log(e)
+        }
+    }
     return(
         <div className="page">
             <header>List your bike</header>
@@ -43,7 +30,7 @@ const NewBikePage = (props)=>{
             <form className="form-horizontal" onSubmit={postData} >
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='name' value={props.bikeData.name} />
+                        <input type='text' placeholder='name' value={postData.bikes} />
                     </div>
                 </div>
                 <div className="form-group">

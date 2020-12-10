dd24d14eb361121cb3cb38ebd442f5aea5671c6a
jared Long <jlong4223@gmail.com>
2020-12-10 12:39:48 -0600

    adds links and styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,10 @@ import './Header.css'
 import {Link} from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
 
+
+
 const Header=(props)=>{
+    
     return(
         <header className="Header">
             <Link to='/'>

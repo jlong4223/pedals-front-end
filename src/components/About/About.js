25b1eb317df0f51ffc32a2226d5107cc81024289
jared Long <jlong4223@gmail.com>
2020-12-10 10:01:47 -0600

    updates styles and backgrounds

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import './About.css'
 const About = (props)=>{
     return(
         <div>
-            <h1 className="aboutHeader">Put your feet on the Pedals and hit the Trails</h1>
+            <h1 className="aboutHeader">Saddle up and hit the Trails</h1>
             <p className="aboutp">Book unforgettable bikes from trusted hosts in Austin, TX</p>
         </div>
     )

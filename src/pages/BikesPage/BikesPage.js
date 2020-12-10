ec45bd969a2090589d848c5eecfde6b75993a13f
jared Long <jlong4223@gmail.com>
2020-12-10 12:40:16 -0600

    adds more bike details to listings

--- src/pages/BikesPage/BikesPage.js
+++ src/pages/BikesPage/BikesPage.js
@@ -3,8 +3,13 @@ import './BikesPage.css'
 const BikesPage = (props)=>{
     return(
         <div className="bikesPage">
-            <h1>{props.bikes.name}</h1>
+            <div className="bikesCard">
+            <h2>{props.bikes.name}</h2>
             <img className="bikeimg" src={props.bikes.imgURL} alt="bike" />
+            <h5>{props.bikes.type}</h5>
+            <h5>{props.bikes.size}</h5>
+            <h6>Contact: <em>name</em></h6>
+            </div>
         </div>
     )
 }

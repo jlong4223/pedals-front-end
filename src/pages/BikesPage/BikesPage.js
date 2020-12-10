28db92c37c88fb6af9873aa664c3bf097e5ee9e0
jared Long <jlong4223@gmail.com>
2020-12-10 13:10:11 -0600

    updates hover styles

--- src/pages/BikesPage/BikesPage.js
+++ src/pages/BikesPage/BikesPage.js
@@ -6,6 +6,7 @@ const BikesPage = (props)=>{
             <div className="bikesCard">
             <h2>{props.bikes.name}</h2>
             <img className="bikeimg" src={props.bikes.imgURL} alt="bike" />
+            <h5>{props.bikes.brand}</h5>
             <h5>{props.bikes.type}</h5>
             <h5>{props.bikes.size}</h5>
             <h6>Contact: <em>name</em></h6>

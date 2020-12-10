7745cfdc2f694ea4eca45d8e2031083204a6ccb2
jared Long <jlong4223@gmail.com>
2020-12-10 15:27:00 -0600

    create bike form working properly and updates page

--- src/pages/NewBikePage/NewBikePage.js
+++ src/pages/NewBikePage/NewBikePage.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import{addBikeData} from '../../services/bike-api'
 
@@ -5,6 +6,18 @@ import{addBikeData} from '../../services/bike-api'
 
 
 const NewBikePage = (props)=>{
+    
+    const [formState, setFormState]= useState([{
+        name: '',
+        type: '',
+        location: '',
+        brand: '',
+        size: '',
+        availableTill: '',
+        imgURL: ''
+      }])
+
+    
     async function postData(e,bikes){  
         e.preventDefault()
         try{
@@ -23,44 +36,63 @@ const NewBikePage = (props)=>{
             console.log(e)
         }
     }
+
+    function handleChange(e) {
+        setFormState(prevState => ({
+            ...prevState,
+            // Using Computed Property Names
+          [e.target.name]: e.target.value
+        }));
+      }
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        try {
+            await addBikeData(formState);
+            // props.history.push('/');
+          } catch (e) {
+            console.log(postData)
+          }
+    }
+
     return(
         <div className="page">
             <header>List your bike</header>
-            {/* add onSubmit to form */}
-            <form className="form-horizontal" onSubmit={postData} >
+            {/* <form className="form-horizontal" onSubmit={postData} > */}
+            <form className="form-horizontal" onSubmit={handleSubmit} >
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='name' value={postData.bikes} />
+                        <input type='text' placeholder='name' name='name' value={formState.name} onChange={handleChange} />
                     </div>
                 </div>
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='type' value={props.bikeData.type}/>
+                        <input type='text' placeholder='type' name='type' value={props.bikeData.type} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='location' value={props.bikeData.location} />
+                        <input type='text' placeholder='location' name='location' value={props.bikeData.location} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='brand' value={props.bikeData.brand}/>
+                        <input type='text' placeholder='brand' name='brand' value={props.bikeData.brand} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='size' value={props.bikeData.size} />
+                        <input type='text' placeholder='size' name='size' value={props.bikeData.size} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='availableTill' value={props.bikeData.availableTill} />
+                        <input type='text' placeholder='availableTill' name='availableTill' value={props.bikeData.availableTill} onChange={handleChange} />
                     </div>
                 </div>
                 <div className="form-group">
                     <div className="col-sm-12">
-                        <input type='text' placeholder='imgURL' value={props.bikeData.imgURL} />
+                        <input type='text' placeholder='imgURL' name='imgURL' value={props.bikeData.imgURL} onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="form-group">

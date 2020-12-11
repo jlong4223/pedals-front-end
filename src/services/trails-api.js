ca54695eaa05134fa91d1a75531941d7a97319d2
jared Long <jlong4223@gmail.com>
2020-12-10 23:19:11 -0600

    splits api link into 3 variables

--- src/services/trails-api.js
+++ src/services/trails-api.js
@@ -2,8 +2,12 @@
 
 // https://www.trailrunproject.com/data/get-trails?lat=30.266666&lon=-97.733330&maxDistance=15&key=200990499-1eb59238be8d3db70bcc9b015c027e40
 
-const BASE_URL = "https://www.trailrunproject.com/data/get-trails?lat=30.266666&lon=-97.733330&maxDistance=15&key=200990499-1eb59238be8d3db70bcc9b015c027e40"
+const BASE_URL = "https://www.trailrunproject.com/data/get-trails?"
+
+const LATLON ='lat=30.266666&lon=-97.733330'
+const DISTANCE = '&maxDistance=30'
+const KEY ='&key=200990499-1eb59238be8d3db70bcc9b015c027e40'
 
 export function fetchTrailData(){
-    return fetch(BASE_URL).then(res=>res.json())
+    return fetch(BASE_URL + LATLON + DISTANCE + KEY).then(res=>res.json())
 }

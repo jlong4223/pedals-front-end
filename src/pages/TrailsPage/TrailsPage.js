29691af918f3c8781635d03ba1273059d7c08bc1
jared Long <jlong4223@gmail.com>
2020-12-11 09:50:58 -0600

    adds link to each trail info page

--- src/pages/TrailsPage/TrailsPage.js
+++ src/pages/TrailsPage/TrailsPage.js
@@ -3,6 +3,10 @@ import "./TrailsPage.css"
 const TrailsPage = (props)=>{
     return(
         <div className="entirepage">
+                {/* <header className='infologo'>
+                <a href={props.trails.url} target="_blank" rel="noreferrer"><i className="fas fa-info-circle"></i></a>
+                </header> */}
+                <a href={props.trails.url} target='_blank' rel='noreferrer'>
         <div className="TrailsPage">
             <div className="picandname">
                 <h1 className="trailname">{props.trails.name}</h1>
@@ -13,8 +17,10 @@ const TrailsPage = (props)=>{
                 <h3>{props.trails.length} miles</h3>
                 <h3>{props.trails.summary}</h3>
                 <h4><em>Difficulty: {props.trails.difficulty}</em></h4>
+                
             </div>
         </div>
+        </a>
         </div>
     )
 }

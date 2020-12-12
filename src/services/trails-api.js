a532c01fe024f80549fb46ecd4cdf2bd71377044
jared Long <jlong4223@gmail.com>
2020-12-12 15:34:46 -0600

    adds commented out links for other API endpoints

--- src/services/trails-api.js
+++ src/services/trails-api.js
@@ -11,3 +11,10 @@ const KEY ='&key=200990499-1eb59238be8d3db70bcc9b015c027e40'
 export function fetchTrailData(){
     return fetch(BASE_URL + LATLON + DISTANCE + KEY).then(res=>res.json())
 }
+
+// Other access URLs
+    // by ID: 
+// https://www.trailrunproject.com/data/get-trails-by-id?ids=7015538,7036440,7006663,7000108,7011192&key=200990499-1eb59238be8d3db70bcc9b015c027e40
+
+    // by favorites
+//  https://www.trailrunproject.com/data/get-favorites?email=jlong4223@gmail.com&key=200990499-1eb59238be8d3db70bcc9b015c027e40

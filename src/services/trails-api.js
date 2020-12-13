f5795afc265b9ccc7819ed9a612c4b60e15e0157
jared Long <jlong4223@gmail.com>
2020-12-12 20:44:23 -0600

    installs dotenv

--- src/services/trails-api.js
+++ src/services/trails-api.js
@@ -2,11 +2,13 @@
 
 // https://www.trailrunproject.com/data/get-trails?lat=30.266666&lon=-97.733330&maxDistance=15&key=200990499-1eb59238be8d3db70bcc9b015c027e40
 
-// TODO add dotenv
+
 const BASE_URL = "https://www.trailrunproject.com/data/get-trails?"
 
 const LATLON ='lat=30.266666&lon=-97.733330'
 const DISTANCE = '&maxDistance=30'
+// TODO add dotenv
+// const KEY = process.env.KEY
 const KEY ='&key=200990499-1eb59238be8d3db70bcc9b015c027e40'
 
 export function fetchTrailData(){

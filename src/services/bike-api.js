21436ef3eb31a6bc1b2559723301b528d616487c
Jared Long <jlong4223@gmail.com>
2021-04-05 15:23:10 -0500

    adds delete service and auth headers for creating and deleting

--- src/services/bike-api.js
+++ src/services/bike-api.js
@@ -1,24 +1,32 @@
+import { getToken } from "./tokenService";
+
 // makes AJAX requests to my bike back end
-// const BASE_URL = 'http://localhost:3001/bikes'
-const BASE_URL = 'https://pedals-backend.herokuapp.com/bikes'
+const BASE_URL = "http://localhost:3001/bikes";
+// const BASE_URL = "https://pedals-backend.herokuapp.com/bikes";
 
-export function fetchBikeData(){
-    return fetch(BASE_URL).then(res=>res.json())
+export function fetchBikeData() {
+  return fetch(BASE_URL).then((res) => res.json());
 }
 
-export function addBikeData(bikes){
-    return fetch(BASE_URL,{
-        method: 'POST',
-        headers: new Headers({'Content-Type': 'application/json'}),
-        body: JSON.stringify(bikes)
-    }).then(res => res.json())
+export function addBikeData(bikes) {
+  return fetch(BASE_URL, {
+    method: "POST",
+    headers: new Headers({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + getToken(),
+    }),
+    body: JSON.stringify(bikes),
+  }).then((res) => res.json());
 }
 
-// export function addBikeData(bikes){
-//     const options ={
-//         method: 'POST',
-//         headers: new Headers({'Content-Type': 'application/json'}),
-//         body: JSON.stringify(bikes)
-//     }
-//     fetch(BASE_URL + '/bikes', options).then(res => res.json())
-// }
+export function deleteBike(bike) {
+  return fetch(BASE_URL + `/${bike._id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+      // added authorization header to send the token with the req
+      Authorization: "Bearer " + getToken(),
+    },
+  });
+}

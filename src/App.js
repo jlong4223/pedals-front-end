588b589719153dd2e8cc18300bba16e8c740e718
Jared Long <jlong4223@gmail.com>
2021-04-05 15:23:38 -0500

    passes props to various pages and removes unneccessary props being sent

--- src/App.js
+++ src/App.js
@@ -1,154 +1,193 @@
-import './App.css';
+import "./App.css";
 
 // imports hooks
-import {useState, useEffect} from 'react'
-import {Route, Switch, Redirect} from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 // import DB / API functions
-import {fetchBikeData} from './services/bike-api'
-import {fetchTrailData} from './services/trails-api'
+import { fetchBikeData } from "./services/bike-api";
+import { fetchTrailData } from "./services/trails-api";
 
 // imported components/pages
-import HomePage from './pages/HomePage/HomePage'
-import SignupPage from './pages/SignupPage/SignupPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import DashboardPage from './pages/DashboardPage/DashboardPage'
-import BikesPage from './pages/BikesPage/BikesPage'
-import TrailsPage from './pages/TrailsPage/TrailsPage'
-import NewBikePage from './pages/NewBikePage/NewBikePage'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import HomePage from "./pages/HomePage/HomePage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import BikesPage from "./pages/BikesPage/BikesPage";
+import TrailsPage from "./pages/TrailsPage/TrailsPage";
+import NewBikePage from "./pages/NewBikePage/NewBikePage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 // imported services
-import {getUser, logout} from './services/userService'
+import { getUser, logout } from "./services/userService";
 
 // creates a 404 page function
-function NotFound(){
-  return(
-  <div>
-    <h1>404 not found</h1>
-  </div>
-  )
+function NotFound() {
+  return (
+    <div>
+      <h1>404 not found</h1>
+    </div>
+  );
 }
 
 function App(props) {
-   //============= User Data ============//
-const [userState, setUserState] = useState({ user: getUser() })
- 
-function handleSignupOrLogin(){
-  setUserState({ user: getUser()})
-  //push user to another page
-  props.history.push('/dashboard')
-}
+  //============= User Data ============//
+  const [userState, setUserState] = useState({ user: getUser() });
 
-function handleLogout(){
-  logout()
-  setUserState({user: null})
-  props.history.push('/')
-}
+  function handleSignupOrLogin() {
+    setUserState({ user: getUser() });
+    //push user to another page
+    props.history.push("/dashboard");
+  }
+
+  function handleLogout() {
+    logout();
+    setUserState({ user: null });
+    props.history.push("/");
+  }
 
   //============== Bike Data =============//
-  const [bikeData, setBikeData]= useState([{
-    name: '',
-    type: '',
-    location: '',
-    brand: '',
-    size: '',
-    availableTill: '',
-    imgURL: '', 
-    contact: ''
-  }])
-  async function getData(){
-    const data = await fetchBikeData()
-    setBikeData(data)
+  const [bikeData, setBikeData] = useState([
+    {
+      name: "",
+      type: "",
+      location: "",
+      brand: "",
+      size: "",
+      availableTill: "",
+      imgURL: "",
+      contact: "",
+    },
+  ]);
+  async function getData() {
+    const data = await fetchBikeData();
+    setBikeData(data);
   }
-  useEffect(()=>{
-    getData()
-  },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
-  function handleNewBike(){
-    props.history.push('/dashboard')
+  function handleNewBike() {
+    props.history.push("/dashboard");
   }
   //============== Trail Data =============//
-const [trailData, setTrailData] = useState([{
-  trails:[{
-    name: '',
-    type: '',
-    summary: '',
-    difficulty: '',
-    stars:'',
-    starVotes:'',
-    location:'',
-    url:'',
-    imgSqSmall: "",
-    imgSmall: "",
-    imgSmallMed: "",
-    imgMedium: "",
-    length: null,
-    ascent: null,
-    descent: null,
-    high: null,
-    low: null,
-    longitude: null,
-    latitude: null,
-    conditionStatus: "",
-    conditionDetails: null,
-    conditionDate: ""
-  }], 
-    success: null
-}])
-async function getTrailData(){
-  const trailData = await fetchTrailData()
-  setTrailData(trailData)
-}
-useEffect(()=>{
-  getTrailData()
-},[])
+  const [trailData, setTrailData] = useState([
+    {
+      trails: [
+        {
+          name: "",
+          type: "",
+          summary: "",
+          difficulty: "",
+          stars: "",
+          starVotes: "",
+          location: "",
+          url: "",
+          imgSqSmall: "",
+          imgSmall: "",
+          imgSmallMed: "",
+          imgMedium: "",
+          length: null,
+          ascent: null,
+          descent: null,
+          high: null,
+          low: null,
+          longitude: null,
+          latitude: null,
+          conditionStatus: "",
+          conditionDetails: null,
+          conditionDate: "",
+        },
+      ],
+      success: null,
+    },
+  ]);
+  async function getTrailData() {
+    const data = await fetchTrailData();
+    setTrailData(data);
+  }
+  useEffect(() => {
+    getTrailData();
+  }, []);
 
   return (
     <div className="App">
       <Header user={userState.user} handleLogout={handleLogout} />
       <Switch>
-        <Route exact path="/" render={()=>
-          <HomePage {...props} bikeData={bikeData}/>
-        }/>
-        <Route exact path='/dashboard' render={()=>
-        getUser() ?
-         <DashboardPage />
-         :
-         <Redirect to="/login" />
-        }/>
-        <Route exact path="/signup" render={props => 
+        <Route
+          exact
+          path="/"
+          render={() => <HomePage {...props} bikeData={bikeData} />}
+        />
+        <Route
+          exact
+          path="/dashboard"
+          render={() =>
+            getUser() ? <DashboardPage /> : <Redirect to="/login" />
+          }
+        />
+        <Route
+          exact
+          path="/signup"
+          render={(props) => (
             <SignupPage handleSignupOrLogin={handleSignupOrLogin} {...props} />
-          }/>
-          <Route exact path="/login" render={props => 
+          )}
+        />
+        <Route
+          exact
+          path="/login"
+          render={(props) => (
             <LoginPage handleSignupOrLogin={handleSignupOrLogin} {...props} />
-          }/>
+          )}
+        />
 
-          <Route exact path="/newbike" render={props =>
-            getUser() ?
-            <NewBikePage bikeData={bikeData} setBikeData={setBikeData} handleNewBike={handleNewBike} {...props} />  
-            :
-            <Redirect to='/login' />
-          } />
+        <Route
+          exact
+          path="/newbike"
+          render={(props) =>
+            getUser() ? (
+              <NewBikePage
+                handleNewBike={handleNewBike}
+                getData={getData}
+                userState={userState}
+                {...props}
+              />
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
+        />
 
-          <Route exact path="/bikes" render={props =>
+        <Route
+          exact
+          path="/bikes"
+          render={(props) => (
             <div className="bikes">
-            {bikeData.map((bikes, idx)=>(
-              <BikesPage key={idx} bikes={bikes} />
-            ))}
-            </div>
-          }/>
-          <Route exact path="/trails" render={props =>
-            <div>
-              {trailData.trails.map((trails, idx)=>(
-                <TrailsPage key={idx} trails={trails}/>
+              {bikeData.map((bikes, idx) => (
+                <BikesPage
+                  key={idx}
+                  bikes={bikes}
+                  getData={getData}
+                  userState={userState}
+                />
               ))}
-
             </div>
-          }/>
+          )}
+        />
+        <Route
+          exact
+          path="/trails"
+          render={(props) => (
+            <div>
+              {trailData.trails.map((trails, idx) => (
+                <TrailsPage key={idx} trails={trails} />
+              ))}
+            </div>
+          )}
+        />
 
-          <Route component={NotFound}/>
+        <Route component={NotFound} />
       </Switch>
       <Footer />
     </div>
